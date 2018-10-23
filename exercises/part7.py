0eca08ecf6977106c68b7d84b2c4fb6e33d10ab2
Carl Hinderer <carl.hinderer4@gmail.com>
2018-10-22 20:58:37 -0600

    Did part 7 exercise 3.

--- exercises/part7.py
+++ exercises/part7.py
@@ -52,3 +52,34 @@ def exercise2():
         print('caught IndexError')
     except MyError:
         print('caught MyError')
+
+
+
+
+# Exercise 3
+# Error handling
+#
+# Write a function called safe(func, *pargs, **kargs) that runs any function with any number of 
+#   positional and/or keyword arguments by using the * arbitrary arguments header and call syntax, 
+#   catches any exception raised while the function runs, and prints the exception using the 
+#   exc_info call in the sys module. Then use your safe function to run your oops function from 
+#   exercise 1 or 2. Put safe in a module file called exctools.py, and pass it the oops
+#   function interactively. What kind of error messages do you get? Finally, expand 
+#   safe to also print a Python stack trace when an error occurs by calling the built-in print_exc
+#   function in the standard traceback module; see earlier in this chapter, and consult the
+#   Python library reference manual for usage details. We could probably code safe as a function 
+#   decorator using Chapter 32 techniques, but we’ll have to move on to the next part of the book to 
+#   learn fully how (see the solutions for a preview).
+
+import sys, traceback
+
+def safe(func, *pargs, **kargs):
+    try:
+        func(*pargs, **kargs)
+    except:
+        traceback.print_exc()
+        print('Class:', sys.exc_info()[0])
+        print('Instance:', sys.exc_info()[1])
+
+def exercise3():
+    safe(oops3)
