eb566d5a4e43f76bf633a646d4b5f5af49e02fc0
Carl Hinderer <carl.hinderer4@gmail.com>
2018-10-23 13:48:57 -0600

    Ran module through anaconda PEP8 linter.

--- exercises/mymod.py
+++ exercises/mymod.py
@@ -1,17 +1,22 @@
 def countLines(name):
     return len(lines(name))
 
+
 def countChars(name):
     charcount = 0
-    for line in lines(name): charcount += len(line)
+    for line in lines(name):
+        charcount += len(line)
     return charcount
+
 
 def lines(name):
     return open(name).readlines()
 
+
 def test(name):
     print('Lines: %s' % countLines(name))
     print('Chars: %s' % countChars(name))
+
 
 # Tests
 if __name__ == '__main__':
